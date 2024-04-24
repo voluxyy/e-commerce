67e5822f9dbaa8f4f75ee1593b0c66ac05e9cb92
dflorent <florent.detres@ynov.com>
2024-04-24 17:47:51 +0200

    :construction: Trying to Use Components

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -1,8 +1,19 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-import { routes } from './app.routes';
+
+@Injectable()
+export class ConfigService {
+  constructor(private http: HttpClient) { }
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    importProvidersFrom(HttpClientModule),
+  ]
+  
 };
+
--- front/src/app/components/header/header.component.ts
+++ front/src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +9,19 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.sass'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  title = 'Pixel - Buy Video Games Online';
+  target: string;
 
+
+  constructor(private http: HttpClient) {
+    this.target = "https://127.0.0.1:7094";
+  }
+
+  ngOnInit(): void {
+    this.http.get<any>(this.target+"/api/home")
+    .subscribe(resp => {
+      this.title = resp;
+    });
+  }
 }
