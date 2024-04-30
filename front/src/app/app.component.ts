d41ce3e7e744d75f77b47e194eb81fb26ce94130
zharrow <florent.detres@protonmail.com>
2024-04-30 09:06:57 +0200

    :construction: Starting From Zero Again

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'front';
