553cef1773648c691f7b564f2c9e2dfc67175c9b
zharrow <florent.detres@protonmail.com>
2024-05-07 21:05:50 +0200

    :rocket: #6 Deploy Updates on Dashboard + :art: Styling

--- front/src/app/admin-services.service.ts
+++ front/src/app/admin-services.service.ts
@@ -6,54 +6,68 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AdminServicesService {
-  readonly apiUrl = 'http://localhost:5016';
+  readonly apiUrl = 'http://localhost:5016/api';
 
   constructor(private http: HttpClient) { }
 
   // Games
   getProductList(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl + '/api/Product/all');
+    return this.http.get<any[]>(this.apiUrl + '/Product/all');
   }
 
   addProduct(game: any): Observable<any> {
     const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
-    return this.http.post<any>(this.apiUrl + '/api/Product', game, httpOptions);
+    return this.http.post<any>(this.apiUrl + '/Product', game, httpOptions);
   }
 
   updateProduct(game: any): Observable<any> {
     const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
-    return this.http.put<any>(this.apiUrl + '/api/Product/update/:id', game, httpOptions);
+    return this.http.put<any>(this.apiUrl + '/Product/update/:id', game, httpOptions);
   }
 
   deleteProduct(gameId: number): Observable<number> {
     const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
-    return this.http.delete<number>(this.apiUrl + '/api/Product/delete/:id' + gameId, httpOptions);
+    return this.http.delete<number>(this.apiUrl + '/Product/delete/:id' + gameId, httpOptions);
   }
 
   // Member Page
   getUserList(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl + '/api/User/all');
+    return this.http.get<any[]>(this.apiUrl + '/User/all');
   }
 
-  updateUser(user: any): Observable<any> {
+  updateUser(userId: number): Observable<any> {
     const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
-    return this.http.put<any>(this.apiUrl + 'employee/UpdateEmployee/', user, httpOptions);
+    return this.http.put<any>(this.apiUrl + '/User/:id', userId, httpOptions);
   }
 
   deleteUser(userId: number): Observable<number> {
     const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
-    return this.http.delete<number>(this.apiUrl + 'employee/DeleteEmployee/' + userId, httpOptions);
+    return this.http.delete<number>(this.apiUrl + '/User/:id' + userId, httpOptions);
   }
 
   // Dashboard
-  // get number of members
-  // get number of sales
-  // get number of the sales on the last 7 days
-  // total site revenues
-  // site revenues on the last 7 days
+  getNumberOfMembers(): Observable<any> {
+    return this.http.get<any>(this.apiUrl + '/User/count');
+  }
+
+  getNumberOfSales(): Observable<any> {
+    return this.http.get<any>(this.apiUrl + '/Sale/count');
+  }
+
+  getNumberOfSalesLast7Days(): Observable<any> {
+    return this.http.get<any>(this.apiUrl + '/Sale/count/7');
+  }
+
+  getTotalSiteRevenues(): Observable<any> {
+    return this.http.get<any>(this.apiUrl + '/Sale/revenue');
+  }
+
+  getSiteRevenuesLast7Days(): Observable<any> {
+    return this.http.get<any>(this.apiUrl + '/Sale/revenue/7');
+  }
 
   // Photo
   uploadPhoto(photo: any) {
-    return this.http.post(this.apiUrl + '/api/User/:id', photo);
+    return this.http.post(this.apiUrl + '/User/:id', photo);
   }
 }
--- front/src/app/header/header.component.ts
+++ front/src/app/header/header.component.ts
@@ -9,5 +9,4 @@ import { RouterModule } from '@angular/router';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
 }
