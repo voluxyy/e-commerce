8f18eb59d74ab22905556c8996708b68d20a6bb9
zharrow <florent.detres@protonmail.com>
2024-05-17 21:23:52 +0200

    feat: Product Page Updates

--- front/src/app/product/product.component.ts
+++ front/src/app/product/product.component.ts
@@ -1,7 +1,8 @@
 import { NgFor, NgIf } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -11,15 +12,75 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
-export class ProductComponent implements OnInit{
-  prod: any;
+export class ProductComponent {
+  productUrl: string;
+  commentsUrl: string;
+  rateUrl: string;
+  userUrl: string;
 
-  constructor(private route: ActivatedRoute) { }
+  comments: any;
+  rates: any;
+  product: any;
 
+  id: number | undefined;
+
+  userComment: [
+    {
+      user: string,
+      title: string,
+      rate: number,
+    }] | null ;
+
+
+  private routeSub: Subscription = new Subscription;
+
+  constructor(private http: HttpClient, private route: ActivatedRoute) {
+    this.productUrl = 'http://localhost:5016/api/Product';
+    this.commentsUrl = 'http://localhost:5016/api/Comment';
+    this.rateUrl = 'http://localhost:5016/api/Rate';
+    this.userUrl = 'http://localhost:5016/api/User';
+    this.userComment = null;
+  }
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      this.prod = params['id'];
+    this.routeSub = this.route.params.subscribe(params => {
+      this.id = params['id'];
     });
+    this.http.get<any>(this.productUrl + '/get/' + this.id)
+      .subscribe(data => {
+        this.product = data;
+      });
+    this.http.get<any>(this.commentsUrl + '/get/' + this.id)
+      .subscribe(data => {
+        this.comments = data;
+      });
+    this.http.get<any>(this.rateUrl + '/get/' + this.id)
+      .subscribe(data => {
+        this.rates = data;
+      });
+    
+
+    for (let comment of this.comments) {
+      let currentRate: any;
+      let currentUser: any;
+      for (let rate of this.rates) {
+        if (rate.UserId == comment.UserId) {
+          currentRate = rate;
+        }
+      this.http.get<any>(this.userUrl + '/get/' + comment.UserId)
+      .subscribe(data => {
+        currentUser = data;
+      });
+      }
+      this.userComment.push({
+        user: currentUser,
+        title: comment.Title,
+        rate: currentRate
+      });
+    }
+
+  }
+  ngOnDestroy() {
+    this.routeSub.unsubscribe();
   }
 }
