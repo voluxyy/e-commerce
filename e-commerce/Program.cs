335bbf6f3808d060a91580fb4c39ee395ccae4c9
voluxyy <lasserre.anael2@gmail.com>
2024-05-19 22:44:43 +0200

    fix: remove useless tables and fix searchbar error

--- e-commerce/Program.cs
+++ e-commerce/Program.cs
@@ -29,10 +29,8 @@ builder.Services.AddDbContext<DataContext>();
 
 // Link repositories
 builder.Services.AddTransient<IAdminRepository, AdminRepository>();
-builder.Services.AddTransient<ICommentRepository, CommentRepository>();
 builder.Services.AddTransient<IProductListRepository, ProductListRepository>();
 builder.Services.AddTransient<IProductRepository, ProductRepository>();
-builder.Services.AddTransient<IRateRepository, RateRepository>();
 builder.Services.AddTransient<IShoppingCartRepository, ShoppingCartRepository>();
 builder.Services.AddTransient<IUserRepository, UserRepository>();
 builder.Services.AddTransient<IWishRepository, WishRepository>();
@@ -41,10 +39,8 @@ builder.Services.AddTransient<IReviewRepository, ReviewRepository>();
 
 // Link services
 builder.Services.AddTransient<IAdminService, AdminService>();
-builder.Services.AddTransient<ICommentService, CommentService>();
 builder.Services.AddTransient<IProductListService, ProductListService>();
 builder.Services.AddTransient<IProductService, ProductService>();
-builder.Services.AddTransient<IRateService, RateService>();
 builder.Services.AddTransient<IShoppingCartService, ShoppingCartService>();
 builder.Services.AddTransient<IUserService, UserService>();
 builder.Services.AddTransient<IWishService, WishService>();
