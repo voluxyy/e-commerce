c60c16a5c01a96f574baff3e2952813a4ced46a9
zharrow <florent.detres@protonmail.com>
2024-05-23 21:40:05 +0200

    feat: searchbar

--- front/src/app/Components/header/header.component.ts
+++ front/src/app/Components/header/header.component.ts
@@ -1,8 +1,9 @@
-import {  HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { lastValueFrom } from 'rxjs';
+import { LinkHTMLAttributes } from 'vue';
 
 @Component({
   selector: 'app-header',
@@ -43,15 +44,40 @@ export class HeaderComponent {
     const container = document.getElementById("searchbar-result-container") as HTMLElement;
 
     container.innerHTML = ''; // Clear previous results
+    container.style.border = '1px solid var(--var-grey)';
 
-    this.result.forEach((product: { name: any; price: any; }) => {
-      const productElement = document.createElement('div');
+    this.result.forEach((product: { name: any; price: any; imagePath: any; id: any }) => {
+      const productElement = document.createElement('a');
+      productElement.href = '/product/' + product.id;
       productElement.className = 'search-result-item';
-      productElement.innerHTML = `
-        <h3>${product.name}</h3>
-        <p>${product.price} €</p>
+
+      productElement.style.width = '100%';
+      productElement.style.display = 'flex';
+      productElement.style.flexDirection = 'column';
+      productElement.style.padding = '1% 4%';
+      productElement.style.textDecoration = 'none';
+      productElement.style.color = 'var(--var-grey)';
+      productElement.style.padding = '20% 4%';
+
+      const imageContainer = document.createElement('div');
+      imageContainer.style.width = '210px';
+      imageContainer.style.height = '180px';
+      
+      const imageElement = document.createElement('img');
+      imageElement.src = '../assets/' + product.imagePath;
+      imageElement.style.width = '100%';
+      imageElement.style.height = '100%';
+      imageElement.style.borderRadius = '20px';
+      imageElement.style.objectFit = 'cover';
+
+      imageContainer.appendChild(imageElement);
+      productElement.appendChild(imageContainer);
+      productElement.innerHTML += `
+        <h3 style="margin:0;padding:0;word-break: break-all; font-size:1rem;">${product.name}</h3>
+        <p style="margin:0;padding:0;">${product.price} €</p>
       `;
       container.appendChild(productElement);
+      
     });
   }
 }
